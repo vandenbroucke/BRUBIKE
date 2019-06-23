0087a0e13ab58756095d164b8e1004e9e93a1935
Luis Manuel Vidal Pina <lmvidal@uci.cu>
2019-06-23 13:02:31 +0200

    Organizing the code on some functions, and evaluationg with data 22 june(last cell)

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 def check_if_the_tsv_hot_file_already_has_processed_columns(csv_file_path):
 	df = pd.read_csv(csv_file_path,delimiter="\t")
 	try:
-		total_rows=len(df.Rain)
+		total_rows=len(df.Sunny)
 		return True
 	except Exception as e:
 		return False	
